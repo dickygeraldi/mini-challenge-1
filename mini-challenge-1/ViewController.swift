42b2e434b17eb98ab86c491e2e848ea14dd5f360
dickygeraldi <dickygeraldi@gmail.com>
2020-04-03 11:16:34 +0700

    Initial Commit

---
+++ mini-challenge-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mini-challenge-1
+//
+//  Created by Dicky Geraldi on 03/04/20.
+//  Copyright © 2020 Dicky Geraldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
