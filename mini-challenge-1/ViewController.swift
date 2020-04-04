f51b3c75732e7f89395f1b4c0096780881864fe1
dickygeraldi <dickygeraldi@gmail.com>
2020-04-04 12:56:04 +0700

    core data Updated

--- mini-challenge-1/ViewController.swift
+++ mini-challenge-1/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        storeDataMessage()
+        checkDataTopic()
     }
+    
+    func checkDataTopic() {
+        guard let appDel = UIApplication.shared.delegate as? AppDelegate else { return }
+        let context = appDel.persistentContainer.viewContext
+        
+        let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
+        var countingDuration : Int = 0
+        
+        do {
+            let result = try context.fetch(fetch)
+            for data in result as! [NSManagedObject] {
+                let duration: Int = data.value(forKey: "duration") as! Int
+                countingDuration += duration
+            }
+        } catch {
+            print("Failed")
+        }
+        
+        print(countingDuration)
+    }
+    
+    func storeDataMessage() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
 
-
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let messageEntity = NSEntityDescription.entity(forEntityName: "Task", in: context)!
+        
+        let listMessage = NSManagedObject(entity: messageEntity, insertInto: context)
+        
+        listMessage.setValue("id", forKey: "id")
+        listMessage.setValue("GoalsId", forKey: "goalId")
+        listMessage.setValue("taskName", forKey: "taskName")
+        listMessage.setValue("start", forKey: "start")
+        listMessage.setValue(60, forKey: "duration")
+        listMessage.setValue(60, forKey: "distraction")
+        listMessage.setValue(true, forKey: "status")
+        
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print("Gagal save context \(error), \(error.userInfo)")
+        }
+    }
 }
 
