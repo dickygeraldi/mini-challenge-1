fd2076a10d3b977f3a1c2ffdd7c6a7059166ddda
NathanaelAdolf <55471987+NathanaelAdolf@users.noreply.github.com>
2020-04-05 11:05:15 +0700

    lanjutin halaman report
    halaman report udah kelar.
    - tinggal masih bingung cara masukin data tanggal ke tabel Goal. padahal data tanggal hari saat aplikasi sedang dibuka g masalah. saat mau masukin ke tabelnya, terjadi error.

--- mini-challenge-1/ReportViewController.swift
+++ mini-challenge-1/ReportViewController.swift
@@ -7,18 +7,105 @@
 //
 
 import UIKit
+import CoreData
 
 class ReportViewController: UIViewController {
 
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var totalGoalsCompletedLabel: UILabel!
+    @IBOutlet weak var totalTaskCompletedLabel: UILabel!
+    @IBOutlet weak var totalProductiveMinsLabel: UILabel!
+    @IBOutlet weak var totalTimesDistractedLabel: UILabel!
+    
+    var countingRow : Int = 0
+    var tempDate = ""
+    var countingTaskRow : Int = 0
+    var countingDuration : Int = 0
+    var countingTaskDistracted : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        hideKeyboardWhenTappedAround()
         // Do any additional setup after loading the view.
+        
+        //method buat ambil data dari coredata
+        checkGoalData()
+        checkTaskData()
+        
+        //set label yang ada di UI
+        totalGoalsCompletedLabel.text = "\(countingRow)"
+        totalTaskCompletedLabel.text = "\(countingTaskRow)"
+        totalProductiveMinsLabel.text = "\(countingDuration)"
+        totalTimesDistractedLabel.text = "\(countingTaskDistracted)"
+        
+        dateLabel.text = tempDate
         
         self.navigationController?.isNavigationBarHidden = true //untuk hilangin navigation bar
     }
     
-
+    func checkGoalData() {
+        guard let appDel = UIApplication.shared.delegate as? AppDelegate else { return }
+        let context = appDel.persistentContainer.viewContext
+        
+        let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Goal")
+        countingRow = 0
+     
+        do {
+            let result = try context.fetch(fetch)
+            for data in result as! [NSManagedObject] {
+             countingRow = countingRow + 1
+             print("Goal table row \(countingRow)")
+             print(data.value(forKey: "id") as! String)
+             print(data.value(forKey: "goalName") as! String)
+             print(data.value(forKey: "date") as! String)
+             print(data.value(forKey: "status") as! boolean_t)
+            }
+        } catch {
+            print("Failed")
+        }
+     print("Total number of row : \(countingRow)")
+    }
+    
+    func checkTaskData() {
+           guard let appDel = UIApplication.shared.delegate as? AppDelegate else { return }
+           let context = appDel.persistentContainer.viewContext
+            countingTaskRow = 0 //menghitung jumlah baris yang ada
+            countingTaskDistracted = 0 //menghitung jumlah berapa kali ke distract yang ada
+            countingDuration = 0 //menghitung jumlah waktu produktif
+        
+           let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
+           
+           do {
+               let result = try context.fetch(fetch)
+               for data in result as! [NSManagedObject] {
+                   countingTaskRow = countingTaskRow + 1
+                   let duration: Int = data.value(forKey: "duration") as! Int
+                  let timesDistracted: Int = data.value(forKey: "distraction") as! Int
+                
+                   countingDuration += duration
+                countingTaskDistracted += timesDistracted
+               }
+           } catch {
+               print("Failed")
+           }
+           
+            print(countingDuration)
+            print(countingTaskRow)
+            print(countingTaskDistracted)
+       }
+    
+    @IBAction func saveReflectionAction(_ sender: Any) {
+        showAlert()
+    }
+    
+    func showAlert() {
+               let alert = UIAlertController(title: "Message", message: "Reflection Saved", preferredStyle: .alert)
+               let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+               alert.addAction(action)
+               self.present(alert, animated: true, completion: nil)
+           }
+    
     /*
     // MARK: - Navigation
 
@@ -29,4 +116,20 @@ class ReportViewController: UIViewController {
     }
     */
 
+}
+
+extension UIViewController
+{
+
+    func hideKeyboardWhenTappedAround()
+    {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.endEditingKeyboard))
+        view.addGestureRecognizer(tap)
+        tap.cancelsTouchesInView = false
+    }
+    
+    @objc func endEditingKeyboard()
+    {
+        view.endEditing(true)
+    }
 }
--- mini-challenge-1/ViewController.swift
+++ mini-challenge-1/ViewController.swift
@@ -14,16 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     
     let dateFormat1: DateFormatter = DateFormatter ()
+    var dateString : String = ""
+    var dateString2 : String = "" //untuk format date yang beda di halaman Report
+     let dateFormat2: DateFormatter = DateFormatter ()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        //storeDataMessage()
+        hideKeyboardWhenTappedAround()
         
         self.navigationController?.isNavigationBarHidden = true //untuk hilangin navigation bar
         showDate()//untuk set dateLabel jadi tanggal hari ini
-        checkDataTopic()
+
+        checkGoalData()
     }
     
     @IBAction func myUnwindAction(unwindSegue:UIStoryboardSegue)
@@ -35,14 +38,53 @@ class ViewController: UIViewController {
        {
            let todayDate = Date()
            dateFormat1.dateFormat = "dd/MM/yy"
-           let dateString = dateFormat1.string(from: todayDate)
-           
+            dateFormat2.dateFormat = "dd MMM yyyy"
+            dateString = dateFormat1.string(from: todayDate)
+            dateString2 = dateFormat2.string(from: todayDate)
+        
            dateLabel.text = dateString
+       }
+    
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if let destination = segue.destination as? ReportViewController
+                   {
+                    destination.tempDate = dateString2
+                    
+
+                   }
+        }
+        
+    }
+
+func storeGoalData()
+       {
+        //memasukkan data ke table goal
+           guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+
+           let context = appDelegate.persistentContainer.viewContext
+           
+           let messageEntity = NSEntityDescription.entity(forEntityName: "Goal", in: context)!
+           
+           let listMessage = NSManagedObject(entity: messageEntity, insertInto: context)
+           
+           listMessage.setValue("G001", forKey: "id")
+           listMessage.setValue("Do Homework", forKey: "goalName")
+           listMessage.setValue("05/04/2020", forKey: "date") //masih belum bisa simpen nilai dari tanggal sekarang(variable dateString),saat dimasukin, gtw kenapa masih eror. padahal udah bisa ngambil data secara otomatis buat sekarang
+           listMessage.setValue(1, forKey: "status")
+           
+           do {
+               try context.save()
+               print("Success save data")
+           } catch let error as NSError
+           {
+               print("Gagal save context \(error), \(error.userInfo)")
+           }
        }
     
     func checkDataTopic() {
         guard let appDel = UIApplication.shared.delegate as? AppDelegate else { return }
         let context = appDel.persistentContainer.viewContext
+        var countingRow : Int = 0 //menghitung jumlah baris yang ada
         
         let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
         var countingDuration : Int = 0
@@ -50,6 +92,7 @@ class ViewController: UIViewController {
         do {
             let result = try context.fetch(fetch)
             for data in result as! [NSManagedObject] {
+                countingRow = countingRow + 1
                 let duration: Int = data.value(forKey: "duration") as! Int
                 countingDuration += duration
             }
@@ -58,7 +101,31 @@ class ViewController: UIViewController {
         }
         
         print(countingDuration)
+        print(countingRow)
     }
+
+func checkGoalData() {
+       guard let appDel = UIApplication.shared.delegate as? AppDelegate else { return }
+       let context = appDel.persistentContainer.viewContext
+       
+       let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Goal")
+        var countingRow : Int = 0
+    
+       do {
+           let result = try context.fetch(fetch)
+           for data in result as! [NSManagedObject] {
+            countingRow = countingRow + 1
+            print("Goal table row \(countingRow)")
+            print(data.value(forKey: "id") as! String)
+            print(data.value(forKey: "goalName") as! String)
+            print(data.value(forKey: "date") as! String)
+            print(data.value(forKey: "status") as! boolean_t)
+           }
+       } catch {
+           print("Failed")
+       }
+    print("Total number of row : \(countingRow)")
+   }
     
     func storeDataMessage() {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
@@ -82,6 +149,10 @@ class ViewController: UIViewController {
         } catch let error as NSError {
             print("Gagal save context \(error), \(error.userInfo)")
         }
-    }
+        
+       
 }
+
+
+
 
